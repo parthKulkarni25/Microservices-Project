01468d768f169f202ae627d3c4ee8ae904934774
parthKulkarni25 <111442583+parthKulkarni25@users.noreply.github.com>
2024-12-05 12:33:22 +0530

    Update StockService.java

--- Stock-UI-App/src/main/java/in/ineuron/service/StockService.java
+++ Stock-UI-App/src/main/java/in/ineuron/service/StockService.java
@@ -1,14 +1,25 @@
 package in.ineuron.service;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import in.ineuron.entity.StockPrice;
+import in.ineuron.entity.StockFundamentalsDTO;
+
 @Service
 public class StockService {
+	
+	@Autowired
+	private WebClient webClient;
+	
 	public String getTotalPrice(String companyName,Integer quantity) {
 		String response = null;
-		WebClient webClient= WebClient.create();
 		String REST_END_POINT = "http://localhost:1111/api/calc/calculate/{companyName}/{quantity}";
 		try {
 			response= webClient.get().uri(REST_END_POINT,companyName,quantity).
@@ -23,7 +34,7 @@ public class StockService {
 	
 	public String addStock(String companyName,Double stockPrice) {
 		String response = null;
-		WebClient webClient = WebClient.create();
+		
 		String REST_END_POINT = "http://localhost:2222/api/price/addStock/{companyName}/{stockPrice}";
 		try {
 			response = webClient.post().uri(REST_END_POINT,companyName,stockPrice)
@@ -36,5 +47,76 @@ public class StockService {
 		}
 		
 		return response;
+	}
+	
+	public List<StockPrice> getCompanyStocks(){
+		String REST_END_POINT = "http://localhost:2222/api/price/listStock";
+		try {
+			return webClient.get().uri(REST_END_POINT)
+					.retrieve()
+					.bodyToMono(new ParameterizedTypeReference<List<StockPrice>>() {})
+					.block();
+					
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}	
+	}
+	
+	public StockPrice getCompanybyId(Integer stockId) {
+		String REST_END_POINT = "http://localhost:2222/api/price/getStockbyId/{stockId}";
+		try {
+			return webClient.get().uri(REST_END_POINT,stockId)
+					.retrieve()
+					.bodyToMono(StockPrice.class)
+					.block();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+	
+	public StockFundamentalsDTO getStockFundamentals(Integer stockId) {
+		String REST_END_POINT = "http://localhost:2222/api/price/getFundamentals/{stockId}";
+		try {
+			return webClient.get().uri(REST_END_POINT,stockId)
+					.retrieve()
+					.bodyToMono(StockFundamentalsDTO.class)
+					.block();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+	
+	public String addStockFundamentals(StockFundamentalsDTO fundamentals,StockPrice stock) {
+		String REST_END_POINT = "http://localhost:2222/api/price/addFundamentals";
+		try {
+			return webClient.post().uri(REST_END_POINT)
+					.contentType(MediaType.APPLICATION_JSON)
+					.bodyValue(fundamentals)
+					.retrieve()
+					.bodyToMono(String.class)
+					.block();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Not able to add";
+		}
+	}
+	
+	
+	public String changeStockPrice(StockPrice stock) {
+		String REST_END_POINT = "http://localhost:2222/api/price/editStockPrice";
+		try {
+			return webClient.post().uri(REST_END_POINT)
+					.contentType(MediaType.APPLICATION_JSON)
+					.bodyValue(stock)
+					.retrieve()
+					.bodyToMono(String.class)
+					.block();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Unable to edit";
+		}
 	}
 }
