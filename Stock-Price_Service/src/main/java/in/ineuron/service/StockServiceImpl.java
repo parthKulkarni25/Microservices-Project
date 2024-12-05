abd7dd4b77f957737c599af6228ecd5dd302f2ea
parthKulkarni25 <111442583+parthKulkarni25@users.noreply.github.com>
2024-12-05 12:21:06 +0530

    Update StockServiceImpl.java

--- Stock-Price_Service/src/main/java/in/ineuron/service/StockServiceImpl.java
+++ Stock-Price_Service/src/main/java/in/ineuron/service/StockServiceImpl.java
@@ -1,9 +1,14 @@
 package in.ineuron.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import in.ineuron.dao.IStockDAO;
+import in.ineuron.dao.IStockFundamentalsDAO;
+import in.ineuron.entity.StockFundamentals;
 import in.ineuron.entity.StockPrice;
 import in.ineuron.exception.StockNotFoundException;
 
@@ -12,6 +17,9 @@ public class StockServiceImpl implements IStockService {
 	
 	@Autowired
 	private IStockDAO dao;
+	
+	@Autowired
+	private IStockFundamentalsDAO fundamentalsDao;
 
 	@Override
 	public Double findByCompany(String companyName) {
@@ -36,6 +44,52 @@ public class StockServiceImpl implements IStockService {
 			return "Stock Added Successfully";
 		else
 			return "Failed to Add stock.";
+	}
+
+	@Override
+	public List<StockPrice> getListStocks() {
+		
+		// TODO Auto-generated method stub
+		return dao.findAll();
+	}
+
+	@Override
+	public StockPrice getStockById(Integer stockId) {
+		Optional<StockPrice> optional = dao.findById(stockId);
+		if(optional.isPresent()) {
+			return optional.get();
+		}
+		return null;
+		
+	}
+
+	@Override
+	public StockFundamentals getStockFundamentals(Integer stockId) {
+		Optional<StockFundamentals> optional = fundamentalsDao.findByCompanyStockId(stockId);
+		if(optional.isPresent()){
+			return optional.get();
+		}else
+		  throw new StockNotFoundException("Stock is not found.");
+	}
+
+	@Override
+	public String addStockFundamentals(StockFundamentals fundamentals,StockPrice stock) {
+		StockPrice saveStockPrice = dao.save(stock);
+		fundamentals.setStock(saveStockPrice);
+		StockFundamentals stockFundamentals = fundamentalsDao.save(fundamentals);
+		if(stockFundamentals != null && saveStockPrice !=null)
+			return "Stock Added Successfully";
+		else
+			return "Failed to Add stock.";
+	}
+
+	@Override
+	public String editStockPrice(StockPrice stock) {
+		StockPrice stock1 = dao.save(stock);
+		if(stock1 != null)
+			return "stock edited..";
+		else
+			return "not able to edit";
 	}
 
 }
