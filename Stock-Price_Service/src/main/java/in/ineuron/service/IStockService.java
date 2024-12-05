77d7dde665d954adc271eda60ed6e1a574f4e3f4
parthKulkarni25 <111442583+parthKulkarni25@users.noreply.github.com>
2024-12-05 12:20:33 +0530

    Update IStockService.java

--- Stock-Price_Service/src/main/java/in/ineuron/service/IStockService.java
+++ Stock-Price_Service/src/main/java/in/ineuron/service/IStockService.java
@@ -1,9 +1,19 @@
 package in.ineuron.service;
 
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder.In;
+
+import in.ineuron.entity.StockFundamentals;
 import in.ineuron.entity.StockPrice;
 
 public interface IStockService {
 
 	public Double findByCompany(String companyName);
 	public String addCompanyStock(String companyName,Double companyPrice);
+	public List<StockPrice> getListStocks();
+	public StockPrice getStockById(Integer stockId);
+	public StockFundamentals getStockFundamentals(Integer stockId);
+	public String addStockFundamentals(StockFundamentals fundamentals,StockPrice stock);
+	public String editStockPrice(StockPrice stock);
 }
